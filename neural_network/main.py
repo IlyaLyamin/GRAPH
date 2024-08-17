8b64985585a83d0aeec038a2dab9658f4eda7955
Ilya <liamin.6@yandex.ru>
2024-08-17 22:06:30 +0300

    table creater is ready

--- neural_network/main.py
+++ neural_network/main.py
@@ -1,7 +1,36 @@
 import cv2
 import numpy as np
+import pandas as pd
+import math
+#list in lists = ["A", "B", 3]
+
+def create_table(lists: np.array):# в этом месте можно придумать алгоритм который будет быстрее находить все дороги от заданной точки
+    points = sorted(list(map(str, list(set(lists[:, 0]) | set(lists[:, 1])))))# пока что нам нгадо заполнить только верхний треугольник
+    final_array = np.full((len(points), len(points)), -1)
+    search = points
+    for i in range(1, len(points)):
+        for road in lists:
+            if road[0] == points[i - 1] and road[1] in search:
+                final_array[i - 1, points.index(road[1])] = road[2]# если сразу закнуть в pandas, то можно работать по буквам в колонках   
+                final_array[points.index(road[1]), i - 1] = road[2]
+            if road[1] == points[i - 1] and road[0] in search:
+                final_array[i - 1, points.index(road[0])] = road[2]
+                final_array[points.index(road[0]), i - 1] = road[2]
+        search = search[1:]
+    DataFrame = pd.DataFrame(final_array, index=points, columns=points)
+    print(points)
+    print(DataFrame)# можно было просто выкидывать рёбра
+    DataFrame.to_csv("C:/projects/GRAPH/c_plus_plus_part/table.csv", header=False)
 
 
+def main():
+    lists = np.array([["A", "B", 3], ["B", "C", 6], ["C", "D", 21], ["D", "A", 5]])
+    create_table(lists)
+
+
+if __name__ == "__main__":
+    main()
+"""
 def processing_picture(img: np.ndarray):
     img = cv2.resize(img, (int(img.shape[1] * 2), int(img.shape[0] * 2)))
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # сделали изображение одноканальным (серым)
@@ -28,7 +57,7 @@ def layering(img: np.ndarray):
     img = cv2.merge([b, g, r]) # соединили 3 слоя
     show_picture(img)
 
-"""
+
 #img = cv2.imread("images/14.PNG")
 #cv2.imshow("Results", img)
 #cv2.waitKey(0)# время которое будет показываться картинка
@@ -49,13 +78,12 @@ while True:
         break
 
 
-"""
-img = cv2.imread("images/shortest_rod.png")
+
+img = cv2.imread("C:/projects/GRAPH/neural_network/images/shortest_rod.png")
+print(img.shape)
+print(img)
 #photo = np.zeros((450, 450, 3), dtype="uint8")
 #photo = paint(photo)
 img_gr = processing_picture(img)
 show_picture(img_gr)
-
-
-if __name__ == '__main__':
-    print('PyCharm')
+"""
